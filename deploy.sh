52554a2ac53a077d1008a36a2c848d577b27870f
Greg Quinn <greg.quinn@ssec.wisc.edu>
2026-02-27 14:10:41 -0600

    Finish compose-only deployment
    Also add a vector layer for flight tracks

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 set -euo pipefail
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # ensure we have all env vars needed by both this script and compose.yml
 : ${DEPLOYMENT_DIR?}
 : ${ONEARTH_VERSION?}
-export COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME:-onearth}
-export CONFIG_DIR=${DEPLOYMENT_DIR}/config
 : ${MRF_ARCHIVE_DIR?}
+export FORCE_REDEPLOY=${FORCE_REDEPLOY:-false}
+export COMPOSE_PROJECT_NAME=${COMPOSE_PROJECT_NAME:-onearth}
 
+if [[ -e "${DEPLOYMENT_DIR}" ]]; then
+    if [[ "${FORCE_REDEPLOY}" != "true" ]]; then
+        echo "ERROR: Destination already exists: ${DEPLOYMENT_DIR}"
+        echo "To teardown and redeploy, set FORCE_REDEPLOY=true"
+        exit 1
+    fi
+    ${SCRIPT_DIR}/teardown.sh
+fi
 mkdir ${DEPLOYMENT_DIR}
 
 # generate full config from layer config using script from the onearth repo
@@ -20,5 +29,11 @@ mkdir ${TMP_CONFIG_DIR}/config
 ln -s ${PWD}/layers ${TMP_CONFIG_DIR}/config/
 ( cd ${ONEARTH_SRC}/docker/local-deployment; ./generate-configs.sh -s ${TMP_CONFIG_DIR} -t ${DEPLOYMENT_DIR} epsg4326 epsg3413 )
 
-# render our compose config
-podman compose --profile demo --profile wms --profile reproject config > ${DEPLOYMENT_DIR}/compose.yml
+# copy in color maps and vector styles
+cp -r colormaps ${DEPLOYMENT_DIR}
+cp -r vector-styles ${DEPLOYMENT_DIR}
+cp -r vector-metadata ${DEPLOYMENT_DIR}
+
+# render our compose config and use it to fire everything up
+podman compose config > ${DEPLOYMENT_DIR}/compose.yml
+( cd ${DEPLOYMENT_DIR} && podman compose up -d )
